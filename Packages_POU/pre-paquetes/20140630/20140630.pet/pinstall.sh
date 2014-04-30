0ad8005e1737153b1fdf5c1e3735296e47bbacf9
Fabián Bonetti <mama21mama@mamalibre.com.ar>
2014-04-30 04:01:56 -0300

    migrando a POU

---
+++ Packages_POU/pre-paquetes/20140630/20140630.pet/pinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#------------------------------------------------------------
+# This part of pinstall.sh is created by /usr/local/POUmp
+
+echo "20140630.pet" >> /usr/local/POU/installed_list
+echo "PET File is marked as already installed"
+
+# End of pinstall.sh
+#------------------------------------------------------------
+
---
+++ Packages_POU/pre-paquetes/20140630/20140630.pet/puninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#------------------------------------------------------------
+# This part of puninstall.sh is created by /usr/local/POUmp
+
+sed -i "/20140630.pet/d" /usr/local/POU/installed_list
+echo "PET File is marked as not installed"
+
+# End of puninstall.sh
+#------------------------------------------------------------
+
